40af2b88a8b72ea68eb5ee2adfdb7ac336e97635
kellynichols99 <kellynichols99@gmail.com>
2022-04-16 20:42:51 -0500

    SQL Challenge Update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -32,3 +32,20 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT (title) DESC;
+
+
+--Deliverable 2 Steps 1-11
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+employees.first_name,
+employees.last_name,
+employees.birth_date,
+dept_emp.from_date,
+dept_emp.to_date,
+titles.title
+INTO mentorship_eligibilty
+FROM employees
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+JOIN titles ON titles.emp_no = employees.emp_no
+WHERE (dept_emp.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
